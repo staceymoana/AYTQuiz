b22ef60bc29fbe7820fd9756008574bea7c860c9
charith <20laklira@gmail.com>
2020-10-26 13:42:00 +1300

    saved quiz list completed
    error messages and success message will showing no bugs

--- frontend/src/app/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {Http, Response, RequestOptions, Headers} from '@angular/http';
+interface Alert {
+  type: string;
+  message: string;
+}
+   
+
+const ALERTS: Alert[] = [];
+
+
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
@@ -8,8 +17,17 @@ import {Http, Response, RequestOptions, Headers} from '@angular/http';
 export class AdminDashboardComponent implements OnInit {
 SavedQuizesArray=[];
 hi;
+close(alert: Alert) {
+  this.alerts.splice(this.alerts.indexOf(alert), 1);
+  //location.reload();
+  this.SavedQuizes();
+}
+
+reset() {
+  this.alerts = Array.from(ALERTS);
+}
   constructor(private  http: Http) { }
-  
+  alerts: Alert[];
   private GetQuizes_URL = 'https://nfmrn7h7kk.execute-api.us-east-1.amazonaws.com/dev/admin/admJoshua/getQuizzes?isPublished=false';
   private PublishQuiz_URL;
   private qid;
@@ -53,20 +71,50 @@ this.hi=data;
 .subscribe(
   data => {
     
-    
-  
-    console.log('success', data.status)
- // this.displayError=false;
- // this.routers.navigate(['/dashboard'])
-},
-  error => {
-    if (error.status==401) {
-   //   this.displayError=true;
+    const ALERTS: Alert[] = [{
+      type: 'success',
+      message: 'The Quiz Published Succesfuly',
     }
-    else{}
-    console.log('oops', )
+    ];
+    this.alerts = Array.from(ALERTS);
+    
+    error => {
+      if (error.status==401) {
+      //  this.displayError=true;
+      const ALERTS: Alert[] = [{
+   
+        type: 'danger',
+        message: 'There is an error',
+      }
+      ];
+      this.alerts = Array.from(ALERTS);
+      
+      //console.log('success', error.status)
+      }
+      else{}
+       console.log('oops', )
+       const ALERTS: Alert[] = [{
+   
+        type: 'danger',
+        message: 'There is an error',
+      }
+      ];
+      this.alerts = Array.from(ALERTS);
+      
+      }},
+error => {
+  const ALERTS: Alert[] = [{
+
+    type: 'danger',
+    message: 'Please update Quiz Details',
+  }
+  ];
+  this.alerts = Array.from(ALERTS);
+
 
 }
+
+
 );
   }
 }
