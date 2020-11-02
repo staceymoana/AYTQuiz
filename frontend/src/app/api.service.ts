07d5d235713152fd1cedba3be77ecc1013d2f86e
stacey moana <staaacey84@hotmail.com>
2020-11-03 11:41:55 +1300

    pulled api.service.ts

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -5,11 +5,47 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  private API_login = 'https://nfmrn7h7kk.execute-api.us-east-1.amazonaws.com/dev/admin/validateLogin';
-  private API_getQuizzes = "https://nfmrn7h7kk.execute-api.us-east-1.amazonaws.com/dev/admin";
-  private API_PublishQuiz = "https://nfmrn7h7kk.execute-api.us-east-1.amazonaws.com/dev/admin";
+  private API_login = 'https://3tkxmc5luk.execute-api.us-east-1.amazonaws.com/dev/admin/validateLogin';
+  private API_getQuizzes = "https://3tkxmc5luk.execute-api.us-east-1.amazonaws.com/dev/admin";
+  private API_PublishQuiz = "https://3tkxmc5luk.execute-api.us-east-1.amazonaws.com/dev/admin";
+  private getQuizQuestionsURL = "https://3tkxmc5luk.execute-api.us-east-1.amazonaws.com/dev/admin"
+  private getQuizDetailsURL = "https://3tkxmc5luk.execute-api.us-east-1.amazonaws.com/dev/admin"
+  private getQuestionDetailsURL = "https://3tkxmc5luk.execute-api.us-east-1.amazonaws.com/dev/adminQ"
+  private getAddQuiz_URL = 'https://3tkxmc5luk.execute-api.us-east-1.amazonaws.com/dev/admin/admJoshua/createQuiz';
+  private getUpdateQuiz_URL= "https://3tkxmc5luk.execute-api.us-east-1.amazonaws.com/dev/admin"
+  private getTakeQuizdetailsAPI = 'https://3tkxmc5luk.execute-api.us-east-1.amazonaws.com/dev/participant'
+  private submitQuizAPI = 'https://3tkxmc5luk.execute-api.us-east-1.amazonaws.com/dev/participant/submitQuizAttempt';
+//nfmrn7h7kk
+//3tkxmc5luk
+
   constructor() { }
- 
+  getsubmitQuizAPI() {
+    return this.submitQuizAPI;
+  }
+
+
+  getTakeQuizAPI(qid) {
+  
+    return (`${this.getTakeQuizdetailsAPI}/${qid}/getQuizDetailsPxt`);
+  }
+
+  getUpdateQuizAPI(username,qid) {
+  
+      return (`${this.getUpdateQuiz_URL}/${username}/${qid}/updateQuiz`);
+    }
+  getAddQuizAPI() {
+    return this.getAddQuiz_URL;
+  }
+
+  getQuizQuestionPI() {
+    return this.getQuizQuestionsURL;
+  }
+  getQuizDetailsAPI() {
+    return this.getQuizDetailsURL;
+  }
+  getQuestionDetailsAPI() {
+    return this.getQuestionDetailsURL;
+  }
   getLoginAPI() {
     return this.API_login;
   }
